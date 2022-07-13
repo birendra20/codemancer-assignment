b657d1b9fd8bfaa9fd326b9f8355364ccaf260a0
birendra20 <birendragupta20@gmail.com>
2022-07-13 21:31:24 +0530

    posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Input from "./components/input/Input";
+import Posts from "./components/posts/Posts";
 
 function App() {
   return (
     <div className="App">
       <Input />
+      <Posts />
     </div>
   );
 }
